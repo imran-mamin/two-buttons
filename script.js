7a0b6fd3e97e891b21695490a3ca4409b122598e
Imran Mamin <imran@mm.st>
2022-07-24 13:34:12 +0300

    Add function yesOrNoClass

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ let yes = 0;
 let no = 0;
 let answering = true;
 
+const yesOrNoClass = function () {
+  btns.classList.toggle('no');
+  btns.classList.toggle('yes');
+};
+
 btnYes.addEventListener('click', function () {
   if (answering) {
     btns.classList.remove('no');
     btns.classList.add('yes');
+
     sum += 1;
     yes += 1;
     btnYes.textContent = `Yes ${Math.trunc((yes / sum) * 100)}%`;
@@ -23,6 +29,7 @@ btnNo.addEventListener('click', function () {
   if (answering) {
     btns.classList.remove('yes');
     btns.classList.add('no');
+
     sum += 1;
     no += 1;
 
